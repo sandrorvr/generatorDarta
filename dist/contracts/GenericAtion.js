c635a5b952e7b11f91977160d807186d21b3165c
Sandro srsj. Ribeiro dos Santos Junior <sandrorsj@transalvadornet.pms.ad>
2023-10-30 14:28:50 -0300

    create gitignore

--- dist/contracts/GenericAtion.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GenericAction = void 0;
-class GenericAction {
-    constructor(action) {
-        this.action = action;
-    }
-}
-exports.GenericAction = GenericAction;
--- dist/contracts/GenericModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GenericModel = void 0;
-class GenericModel {
-    constructor(model) {
-        this.model = model;
-    }
-}
-exports.GenericModel = GenericModel;
